c54c84a0b35be5b1e48870485b8f2e0a73eff81c
Bilal Siddiqui <14809572+slmatrix@users.noreply.github.com>
2021-09-20 11:03:47 -0500

    rename files based on regex pattern for BB downloads

---
+++ rename.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+#usage msg
+die "Please provide the file extensions to rename! $!\n" unless $0 > 1;
+
+
+#cmdline parameters: all stored in '@ARGV' with $ARGV[0] = 1st arg, $ARGV[1] = 2nd arg, ...
+@files = `ls *.$ARGV[0]`;  #read all filenames ending with the extension given as the 1st cmdline parameter 
+#print @files;             #check that the correct files were read in
+chop @files;               #remove the newline character from the filenames (it is appended by the ls cmd)
+
+
+$count = 0;
+@new_name;
+foreach $file (@files)
+{
+  if ($file =~ m|_([a-z]{3}[0-9]{3})_|)     #extract each student's abc123 from the filename  
+  {
+    $new_name[$count] = "$1.$ARGV[0]";      #create a new file name, 'abc123.EXTENSION'
+    $count +=1;
+  }
+}
+
+
+$count = 0;
+foreach $file (@files)
+{
+  #CAUTION: check print output before uncommenting the renaming line!
+  #print "mv '$file' $new_name[$count]"; 
+  `mv "$file" $new_name[$count]`;  #rename all files to be be 'abc123.EXTENSION'
+  $count +=1;
+}
+
+
+#remove the redundant submission txt files
+`rm *.txt`;
