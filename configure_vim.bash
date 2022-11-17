391b7180e3eceea9ccf24e34939e10a995c01e22
slmatrix <14809572+slmatrix@users.noreply.github.com>
2022-11-17 10:40:19 -0600

    <major> new vimrc and config script

--- configure_vim.bash
+++ configure_vim.bash
@@ -1,9 +1,12 @@
+cp ./.vimrc ~/
+
 mkdir -p ~/.vim/pack/plugin/opt/
 mkdir -p ~/.vim/pack/plugin/start/
 git clone https://github.com/preservim/nerdtree.git        ~/.vim/pack/plugin/start/nerdtree
 git clone https://github.com/tpope/vim-fugitive.git        ~/.vim/pack/plugin/start/fugitive
 git clone https://github.com/lifepillar/vim-solarized8.git ~/.vim/pack/plugin/opt/solarized8
 git clone https://github.com/vim-airline/vim-airline.git   ~/.vim/pack/plugin/start/vim-airline
-git clone https://github.com/ycm-core/YouCompleteMe.git    ~/.vim/pack/plugin/start/YouCompleteMe && \
-                                                        cd ~/.vim/pack/plugin/start/YouCompleteMe && \
-                                                        python3 install.py --clangd-completer
+git clone https://github.com/airblade/vim-gitgutter.git    ~/.vim/pack/plugin/start/gitgutter
+nvim -u NONE -c "helptags vim-gitgutter/doc" -c q
+
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/plugin/start/ale
